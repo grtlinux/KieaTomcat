e46f100004063e1ab6096191a1871f76fe280ede
grtlinux <grtlinux@gmail.com>
2018-11-29 16:06:30 +0900

    create 1st

---
+++ gradle01/gs-accessing-data-rest/src/main/java/org/tain/kang/PersonRepository.java
@@ -0,0 +1,9 @@
+package org.tain.kang;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+}
