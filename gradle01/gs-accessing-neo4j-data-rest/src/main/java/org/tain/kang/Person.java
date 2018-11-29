90a80c1da49c5238a11c00c0d7d85a8da659068f
grtlinux <grtlinux@gmail.com>
2018-11-30 07:10:24 +0900

    create 1st

---
+++ gradle01/gs-accessing-neo4j-data-rest/src/main/java/org/tain/kang/Person.java
@@ -0,0 +1,34 @@
+package org.tain.kang;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+@NodeEntity
+public class Person {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String firstName;
+	private String lastName;
+	/////////////////////////////////////////
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
