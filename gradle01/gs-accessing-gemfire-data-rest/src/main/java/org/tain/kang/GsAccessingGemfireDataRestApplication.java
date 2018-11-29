799cd1e84b8788c1e9eb519284b20cb85844c661
grtlinux <grtlinux@gmail.com>
2018-11-30 07:32:42 +0900

    create 1st

---
+++ gradle01/gs-accessing-gemfire-data-rest/src/main/java/org/tain/kang/GsAccessingGemfireDataRestApplication.java
@@ -0,0 +1,19 @@
+package org.tain.kang;
+
+import org.apache.geode.cache.client.ClientRegionShortcut;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
+
+@SpringBootApplication
+@ClientCacheApplication(name="AccessingGemFireDataRestApplication", logLevel="error")
+@EnableEntityDefinedRegions(basePackageClasses=Person.class, clientRegionShortcut=ClientRegionShortcut.LOCAL)
+@EnableGemfireRepositories
+public class GsAccessingGemfireDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAccessingGemfireDataRestApplication.class, args);
+	}
+}
---
+++ gradle01/gs-accessing-gemfire-data-rest/src/main/java/org/tain/kang/PersonRepository.java
@@ -0,0 +1,13 @@
+package org.tain.kang;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+	public List<Person> findByLastName(@Param("name") String name);
+}
