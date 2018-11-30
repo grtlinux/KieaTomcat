6b3db1d85a7263668c342d4d03c0afef37ac0b9e
grtlinux <grtlinux@gmail.com>
2018-12-01 08:17:28 +0900

    create 1st

---
+++ gradle01/gs-securing-web/src/main/java/org/tain/kang/GsSecuringWebApplication.java
@@ -0,0 +1,12 @@
+package org.tain.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSecuringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSecuringWebApplication.class, args);
+	}
+}
