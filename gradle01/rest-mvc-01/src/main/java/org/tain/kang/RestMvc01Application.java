ceddf7a5ffa76aa12cd29a197e77c7cef589957b
grtlinux <grtlinux@gmail.com>
2018-12-03 08:11:09 +0900

    create 1st

---
+++ gradle01/rest-mvc-01/src/main/java/org/tain/kang/RestMvc01Application.java
@@ -0,0 +1,12 @@
+package org.tain.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMvc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMvc01Application.class, args);
+	}
+}
