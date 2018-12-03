5a46a7bb221379a25c400b892a3eeb5f42ff975e
grtlinux <grtlinux@gmail.com>
2018-12-03 21:56:08 +0900

    modify

---
+++ maven01/mvn-accessing-data-rest/src/main/java/org/tain/kang/PersonRepository.java
@@ -0,0 +1,13 @@
+package org.tain.kang;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	public List<Person> findByLastName(@Param("name") String name);
+}
