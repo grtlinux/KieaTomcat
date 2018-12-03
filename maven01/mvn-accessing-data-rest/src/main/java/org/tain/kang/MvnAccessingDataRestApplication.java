e72e7ccedc38c359657d1311220d605150ab3d19
grtlinux <grtlinux@gmail.com>
2018-12-03 21:42:10 +0900

    create 1st

---
+++ maven01/mvn-accessing-data-rest/src/main/java/org/tain/kang/MvnAccessingDataRestApplication.java
@@ -0,0 +1,12 @@
+package org.tain.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnAccessingDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnAccessingDataRestApplication.class, args);
+	}
+}
