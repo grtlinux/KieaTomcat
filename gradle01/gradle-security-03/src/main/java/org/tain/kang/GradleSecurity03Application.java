c53d819006511ee664136dc9834928cb1eb950ac
grtlinux <grtlinux@gmail.com>
2018-12-16 04:20:30 +0900

    create 1st

---
+++ gradle01/gradle-security-03/src/main/java/org/tain/kang/GradleSecurity03Application.java
@@ -0,0 +1,14 @@
+package org.tain.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSecurity03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSecurity03Application.class, args);
+	}
+
+}
+
