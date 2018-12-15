faf33b208891d21560bb64911341b9b5b926dd5e
grtlinux <grtlinux@gmail.com>
2018-12-16 02:25:10 +0900

    create 1st

---
+++ gradle01/gradle-security-01/src/main/java/org/tain/kang/GradleSecurity01Application.java
@@ -0,0 +1,14 @@
+package org.tain.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSecurity01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSecurity01Application.class, args);
+	}
+
+}
+
---
+++ gradle01/gradle-security-01/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HelloController</title>
+</head>
+<body>
+	<h2>HelloController: index.jsp</h2><br>
+	The time on the server is ${serverTime}.
+
+</body>
+</html>
