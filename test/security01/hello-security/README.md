f9722445223c5f8d02692cff2066adcc5fb42ad3
grtlinux <grtlinux@gmail.com>
2019-01-03 07:19:46 +0900

    .

--- test/security01/hello-security/README.md
+++ test/security01/hello-security/README.md
@@ -7,7 +7,7 @@ KieaTomcat/test/security01/hello-security
 
 References
 ----------
-- []( ""):
+- [Spring 4 Tutorial with Examples](https://www.concretepage.com/spring-4/ "Spring 4 Tutorial with Examples"): ConcretePage.com ^^
 
 
 
--- test/security01/hello-security/src/main/java/org/tain/kang/WebMvcConfig.java
+++ test/security01/hello-security/src/main/java/org/tain/kang/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
@@ -14,5 +14,8 @@ public class MvcConfig implements WebMvcConfigurer {
 		//registry.addViewController("/").setViewName("home");
 		registry.addViewController("/login").setViewName("login");
 		registry.addViewController("/hello").setViewName("hello");
+		//registry.addViewController("/kang").setViewName("kang/kang");
+		registry.addViewController("/kang/*").setViewName("kang/*");
+		//registry.addViewController("/kang/**").setViewName("kang/**");
 	}
 }
