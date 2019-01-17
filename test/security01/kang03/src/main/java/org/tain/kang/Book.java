1a7a970be8968c79b405407537033ae0a6c83df0
grtlinux <grtlinux@gmail.com>
2019-01-18 04:08:25 +0900

    create 1st

---
+++ test/security01/kang03/src/main/java/org/tain/kang/Book.java
@@ -0,0 +1,14 @@
+package org.tain.kang;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Book {
+
+	private String val1;
+	private List<String> list;
+}
