cfd736973db1bf9e4ba4dd65e85c4ef61486029c
grtlinux <grtlinux@gmail.com>
2019-02-03 01:41:05 +0900

    create 1st

---
+++ test/javafx01/JavaFx01/src/org/tain/verify/exam01/RootController.java
@@ -0,0 +1,13 @@
+package org.tain.verify.exam01;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class RootController implements Initializable {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+}
+
