b34338d30019baa8e2663630a89916c2f8939add
grtlinux <grtlinux@gmail.com>
2019-02-09 23:06:29 +0900

    create 1st

--- test/bean01/BeanTest01/src/main/java/org/tain/account/Account.java
+++ test/bean01/BeanTest01/src/main/java/org/tain/account/Account.java
@@ -1,5 +1,14 @@
 package org.tain.account;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Account {
 
+	private Integer id;
+	private String email;
+	private String password;
+	private String[] roles;
 }
---
+++ test/bean01/BeanTest01/src/main/java/org/tain/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package org.tain.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/", "/home", "/create").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login").permitAll()
+				.and()
+			.logout()
+				.logoutSuccessUrl("/logout").permitAll();
+	}
+	
+	@Override
+	protected UserDetailsService userDetailsService() {
+		@SuppressWarnings("deprecation")
+		UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+		
+		List<UserDetails> users = new ArrayList<>();
+		users.add(userBuilder.username("admin").password("kang").roles("ADMIN").build());
+		users.add(userBuilder.username("user").password("kang").roles("USER").build());
+		
+		return new InMemoryUserDetailsManager(users);
+	}
+}
