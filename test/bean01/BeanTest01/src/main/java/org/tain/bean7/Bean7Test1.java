fdd368681c0cb12e563542e15d3fe15b13a05e11
grtlinux <grtlinux@gmail.com>
2019-02-11 18:07:35 +0900

    modified

--- test/bean01/BeanTest01/src/main/java/org/tain/bean7/Bean7Test1.java
+++ test/bean01/BeanTest01/src/main/java/org/tain/bean7/Bean7Test1.java
@@ -1,17 +1,23 @@
 package org.tain.bean7;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(value = "Bean7Test1")
 public class Bean7Test1 {
 
+	@Autowired
+	private Bean7Test2 bean7Test2;
+	
 	public Bean7Test1() {
 		System.out.println(">>>>> Bean7Test1 constructor");  // OK!!
+		//bean7Test2.hello();  // error
 	}
 	
-	@Bean
+	@Bean(value = "Bean7Test1.hello")
 	public void hello() {
+		bean7Test2.hello();  // OK!!
 		System.out.println(">>>>> Bean7Test1.hello method...");  // OK!!
 	}
 }
