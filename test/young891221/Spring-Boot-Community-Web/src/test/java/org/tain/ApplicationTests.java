fae04b7d0a1705f82f6e1e8ef442cb88c6203865
grtlinux <grtlinux@gmail.com>
2019-02-12 20:06:18 +0900

    add JpaMappingTest.java

--- test/young891221/Spring-Boot-Community-Web/src/test/java/org/tain/ApplicationTests.java
+++ test/young891221/Spring-Boot-Community-Web/src/test/java/org/tain/ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringBootCommunityWebApplicationTests {
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
