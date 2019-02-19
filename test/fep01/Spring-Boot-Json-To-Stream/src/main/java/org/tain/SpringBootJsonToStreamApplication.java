241c820cfc734659de25cc96f28caaa3ba6311d9
grtlinux <grtlinux@gmail.com>
2019-02-19 21:26:41 +0900

    create 1st

---
+++ test/fep01/Spring-Boot-Json-To-Stream/src/main/java/org/tain/SpringBootJsonToStreamApplication.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsonToStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJsonToStreamApplication.class, args);
+	}
+
+}
