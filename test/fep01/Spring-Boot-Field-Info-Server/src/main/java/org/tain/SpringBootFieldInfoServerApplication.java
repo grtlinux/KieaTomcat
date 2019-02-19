1ac53d83ae24a6e8e1a08fde758c7414a4875dd7
grtlinux <grtlinux@gmail.com>
2019-02-19 15:20:08 +0900

    create 1st

---
+++ test/fep01/Spring-Boot-Field-Info-Server/src/main/java/org/tain/SpringBootFieldInfoServerApplication.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFieldInfoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFieldInfoServerApplication.class, args);
+	}
+
+}
