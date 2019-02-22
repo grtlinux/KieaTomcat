fa8e9879d1c0ec8aa037e0ea80227694f0d0cb7b
grtlinux <grtlinux@gmail.com>
2019-02-23 04:51:42 +0900

    create 1st

--- test/fep02/Spring-Boot2-Field-Info-Proxy/src/main/java/org/tain/controller/FieldInfoController.java
+++ test/fep02/Spring-Boot2-Field-Info-Proxy/src/main/java/org/tain/controller/FieldInfoController.java
@@ -1,17 +1,12 @@
 package org.tain.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.tain.fep.fieldInfo.FieldInfo;
 import org.tain.fep.http.FepHttp;
-import org.tain.utils.ClassPathResourceReader;
 import org.tain.utils.ClassUtil;
 
 @RestController
@@ -26,26 +21,22 @@ public class FieldInfoController {
 		return "Hello, world!!!!";
 	}
 	
-	// curl -H 'Content-Type: application/json' -X GET http://localhost:8081/fieldInfo/list
+	// curl -H 'Content-Type: application/json' -X GET http://localhost:8082/fieldInfo/list
 	@GetMapping(value = {"/list"})
-	public List<String> list() {
-		return FieldInfo.getInstance().getList();
+	public String list() throws Exception {
+		return FepHttp.getInstance().get("http://localhost:8081/fieldInfo/list");
 	}
 	
-	// curl -H 'Content-Type: application/json' -X POST -d '{"infoKey":"MastInfo"}' http://localhost:8081/fieldInfo/info
+	// curl -H 'Content-Type: application/json' -X POST -d '{"infoKey":"MastInfo"}' http://localhost:8082/fieldInfo/info
 	//@RequestMapping(value = "/info", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/info", method = RequestMethod.POST)
 	public String info(HttpEntity<String> httpEntity) throws Exception {
-		Map<?,?> map = FepHttp.getInstance().getMap(httpEntity.getBody());
-		return FieldInfo.getInstance().get((String) map.get("infoKey"));
+		return FepHttp.getInstance().post("http://localhost:8081/fieldInfo/info", httpEntity.getBody());
 	}
 	
-	// curl -H 'Content-Type: application/json' -X GET http://localhost:8081/fieldInfo/MastInfo
+	// curl -H 'Content-Type: application/json' -X GET http://localhost:8082/fieldInfo/MastInfo
 	@GetMapping(value = {"/{name}"})
-	public String field(@PathVariable("name") String name) {
-		if (flag) System.out.println(">>>>> " + ClassUtil.getClassInfo());
-		String filePath = String.format("templates/json/%s.json", name);
-		if (flag) System.out.println(">>>>> " + filePath);
-		return new ClassPathResourceReader(filePath).getContent();
+	public String field(@PathVariable("name") String name) throws Exception {
+		return FepHttp.getInstance().get("http://localhost:8081/fieldInfo/" + name);
 	}
 }
