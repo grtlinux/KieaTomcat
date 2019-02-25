9e74f78bc2b0a8a979d78c63ea4e3c48b2c86cb8
grtlinux <grtlinux@gmail.com>
2019-02-26 03:45:15 +0900

    modify some

--- test/fep02/Spring-Boot2-Res-Json-Generator/src/main/java/org/tain/SpringBoot2ResJsonGeneratorApplication.java
+++ test/fep02/Spring-Boot2-Res-Json-Generator/src/main/java/org/tain/SpringBoot2ResJsonGeneratorApplication.java
@@ -1,25 +1,29 @@
 package org.tain;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.tain.fep.http.FepHttp;
 import org.tain.utils.ClassUtil;
 
 @SpringBootApplication
-public class SpringBoot2ResJsonGeneratorApplication {
+public class SpringBoot2ResJsonGeneratorApplication implements CommandLineRunner {
 
 	private static final boolean flag = true;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2ResJsonGeneratorApplication.class, args);
+		if (flag) System.out.println(">>>>> " + ClassUtil.getClassInfo());
 		if (!flag) System.exit(0);
 	}
 	
-	@Bean
-	public void test01() {
+	@Override
+	public void run(String... args) throws Exception {
 		if (flag) System.out.println(">>>>> " + ClassUtil.getClassInfo());
 		
-		//if (flag) System.out.println(">>>>> " + FieldInfo.getInstance().getList());
-		//if (flag) System.out.println(">>>>> " + FieldInfo.getInstance().get("MastInfo"));
+		if (flag) {
+			String json = FepHttp.getInstance().post("http://localhost:8091/resStream/info", "{\"index\": 0}");
+			System.out.println(">>>>> json: " + json);
+		}
 	}
 }
