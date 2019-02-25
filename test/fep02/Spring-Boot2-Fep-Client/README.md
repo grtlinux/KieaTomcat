8d0bf22c03735c1b5efc87942be3e25c79c8f7c2
grtlinux <grtlinux@gmail.com>
2019-02-26 01:22:11 +0900

    create 1st

---
+++ test/fep02/Spring-Boot2-Fep-Client/README.md
@@ -0,0 +1,31 @@
+Spring-Boot2-Fep-Client
+=======================
+
+- Data Rest
+
+
+
+
+
+References
+----------
+- [Java Apache HttpClient REST (RESTful) client examples](https://alvinalexander.com/java/java-apache-httpclient-restful-client-examples ""):
+- [Apache HttpClient GET and POST examples](https://howtodoinjava.com/httpclient/jaxrs-client-httpclient-get-post/ ""):
+- [Angular 6 HttpClient: Consume RESTful API Example](https://www.djamware.com/post/5b87894280aca74669894414/angular-6-httpclient-consume-restful-api-example ""):
+- [iamport-rest-client-java-hc](https://github.com/iamport/iamport-rest-client-java-hc ""):
+- [Apache HttpClient Examples](https://www.mkyong.com/java/apache-httpclient-examples/ ""):
+- [Java Code Examples for org.apache.http.impl.client.HttpClientBuilder](https://www.programcreek.com/java-api-examples/?api=org.apache.http.impl.client.HttpClientBuilder ""):
+- [Java Code Examples for org.apache.http.impl.client.HttpClientBuilder.setSSLSocketFactory()](https://www.programcreek.com/java-api-examples/?class=org.apache.http.impl.client.HttpClientBuilder&method=setSSLSocketFactory ""):
+- [Java Examples for org.apache.http.impl.client.HttpClientBuilder](https://www.javatips.net/api/org.apache.http.impl.client.httpclientbuilder ""):
+- [HttpClient 4 Cookbook](https://www.baeldung.com/httpclient4 ""):
+- [HttpClient 4 Tutorial](https://www.baeldung.com/httpclient-guide ""):
+- []( ""):
+- []( ""):
+- []( ""):
+- []( ""):
+- []( ""):
+- []( ""):
+- []( ""):
+
+
+.....
---
+++ test/fep02/Spring-Boot2-Fep-Client/src/main/java/org/tain/fep/example/server/StreamServer.java
@@ -0,0 +1,54 @@
+package org.tain.fep.example.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.tain.utils.ClassUtil;
+
+@Component(value = "fep.example.server.StreamServer")
+public class StreamServer {
+
+	private static final boolean flag = true;
+	
+	@Autowired
+	private StreamServerProperty streamServerProperty;
+	
+	@Bean(value = "fep.example.server.StreamServer.runner")
+	public void runner() {
+		if (flag) {
+			if (flag) System.out.println(">>>>> " + ClassUtil.getClassInfo());
+			if (flag) System.out.println(">>>>> " + this.streamServerProperty);
+		}
+
+		if (flag) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					// TODO KANG-20190213: base ServerSocket
+					ServerSocket serverSocket = null;
+					
+					try {
+						serverSocket = new ServerSocket();
+						serverSocket.bind(new InetSocketAddress("localhost", streamServerProperty.getListenPort()));
+						while (true) {
+							if (flag) System.out.println(">>>>> waiting for client connection...");
+							Socket socket = serverSocket.accept();
+							new CommunicationThread(socket, streamServerProperty).start();
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					} finally {
+						if (!serverSocket.isClosed()) {
+							try { serverSocket.close(); } catch (IOException e) {}
+						}
+					}
+				}
+			}).start();
+		}
+	}
+}
