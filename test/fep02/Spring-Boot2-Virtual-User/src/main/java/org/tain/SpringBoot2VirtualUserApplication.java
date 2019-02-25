8e6a03fdddf7405e7c9622ca4c01f98a75fb4dee
grtlinux <grtlinux@gmail.com>
2019-02-25 18:59:28 +0900

    create 1st

--- test/fep02/Spring-Boot2-Virtual-User/src/main/java/org/tain/SpringBoot2VirtualUserApplication.java
+++ test/fep02/Spring-Boot2-Virtual-User/src/main/java/org/tain/SpringBoot2VirtualUserApplication.java
@@ -1,13 +1,45 @@
 package org.tain;
 
+import java.util.Map;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.tain.fep.http.FepHttp;
+import org.tain.utils.ClassUtil;
 
 @SpringBootApplication
-public class SpringBoot2VirtualUserApplication {
+public class SpringBoot2VirtualUserApplication implements CommandLineRunner {
 
+	private static final boolean flag = true;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2VirtualUserApplication.class, args);
+		if (flag) System.out.println(">>>>> " + ClassUtil.getClassInfo());
+		if (flag) System.exit(0);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		if (flag) System.out.println(">>>>> " + ClassUtil.getClassInfo());
+
+		String json;
+		
+		//int[] arrIndex = new int[] { 0, 1, 2, 3 };
+		int[] arrIndex = new int[] { 0, 1, 2 };
+		for (int idx : arrIndex ) {
+			json = FepHttp.getInstance().post("http://localhost:8085/reqStream/info", String.format("{\"index\": %d}", idx));
+			if (flag) System.out.println(">>>>> json : " + json);
+			Map<?,?> map = FepHttp.getInstance().getMap(json);
+			if (flag) System.out.println(">>>>> map : " + map);
+			
+//			if ((Integer) map.get("status") != 200) {
+//				if (flag) System.out.println(">>>>> ERROR: " + (String) map.get("message"));
+//				break;
+//			}
+
+			json = FepHttp.getInstance().post("http://localhost:8086/virtualUser/info", json);
+			if (flag) System.out.println(">>>>> json : " + json);
+		}
+	}
 }
