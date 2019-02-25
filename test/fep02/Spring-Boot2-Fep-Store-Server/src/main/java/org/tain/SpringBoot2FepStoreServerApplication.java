17f998891092cedf86ef2620a44e6c5e85ef07d6
grtlinux <grtlinux@gmail.com>
2019-02-26 03:52:43 +0900

    modify some

--- test/fep02/Spring-Boot2-Fep-Store-Server/src/main/java/org/tain/SpringBoot2FepStoreServerApplication.java
+++ test/fep02/Spring-Boot2-Fep-Store-Server/src/main/java/org/tain/SpringBoot2FepStoreServerApplication.java
@@ -1,13 +1,28 @@
 package org.tain;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.tain.fep.http.FepHttp;
+import org.tain.utils.ClassUtil;
 
 @SpringBootApplication
-public class SpringBoot2FepStoreServerApplication {
+public class SpringBoot2FepStoreServerApplication implements CommandLineRunner {
 
+	private static final boolean flag = true;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2FepStoreServerApplication.class, args);
+		if (flag) System.out.println(">>>>> " + ClassUtil.getClassInfo());
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		if (flag) System.out.println(">>>>> " + ClassUtil.getClassInfo());
+		
+		if (flag) {
+			String json = FepHttp.getInstance().post("http://localhost:8093/store/info", "{\"fld001\": 990}");
+			System.out.println(">>>>> json: " + json);
+		}
+	}
 }
