91929d4a1837dc7f17df03b48f4c72ddedfc6b16
grtlinux <grtlinux@gmail.com>
2019-03-02 22:22:53 +0900

    sort desc on idx

--- test/young891221/Spring-Boot-Community-Web/src/main/java/org/tain/service/BoardService.java
+++ test/young891221/Spring-Boot-Community-Web/src/main/java/org/tain/service/BoardService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.tain.domain.Board;
 import org.tain.repository.BoardRepository;
@@ -15,7 +16,10 @@ public class BoardService {
 	private BoardRepository boardRepository;
 	
 	public Page<Board> findBoardList(Pageable pageable) {
-		pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber()-1, pageable.getPageSize());
+		pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber()-1
+				, pageable.getPageSize()
+				, Sort.Direction.DESC
+				, "idx");
 		return this.boardRepository.findAll(pageable);
 	}
 	
